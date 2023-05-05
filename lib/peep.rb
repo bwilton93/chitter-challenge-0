88e2a5240a13c7bed4f0599ec150f2a1dc42b12c
Barney <B.Wilton1993@gmail.com>
2023-05-05 10:22:21 +0100

    Peep_repo can now return a list of all peeps

---
+++ lib/peep.rb
@@ -0,0 +1,3 @@
+class Peep
+  attr_accessor :id, :content, :date, :time, :author_id
+end
--- lib/peep_repo.rb
+++ lib/peep_repo.rb
@@ -1,3 +1,28 @@
-class PeepRepo
+require_relative 'peep'
 
+class PeepRepo
+  def all
+    peeps = []
+    sql = 'SELECT * FROM peeps;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    result_set.each do |record|
+      peeps << new_peep(record)
+    end
+    
+    return peeps
+  end
+  
+  private
+  
+  def new_peep(record)
+    peep = Peep.new
+    peep.id = record['id'].to_i
+    peep.content = record['content']
+    peep.date = record['date']
+    peep.time = record['time']
+    peep.author_id = record['author_id'].to_i
+
+    return peep
+  end
 end
