8896b5070fb03e9376743ed7e4d138e099494148
Barney <B.Wilton1993@gmail.com>
2023-05-05 11:26:59 +0100

    Wrote test for creating a new peep and then code to satisfy test

--- lib/peep_repo.rb
+++ lib/peep_repo.rb
@@ -7,15 +7,22 @@ class PeepRepo
     result_set = DatabaseConnection.exec_params(sql, [])
 
     result_set.each do |record|
-      peeps << new_peep(record)
+      peeps << peep(record)
     end
     
     return peeps.sort_by { |peep| peep.id }.reverse!
   end
+
+  def create(peep)
+    params = [peep.content, peep.date, peep.time, peep.author_id]
+    sql = 'INSERT INTO peeps (content, date, time, author_id) VALUES ($1, $2, $3, $4)'
+
+    DatabaseConnection.exec_params(sql, params)
+  end
   
   private
   
-  def new_peep(record)
+  def peep(record)
     peep = Peep.new
     peep.id = record['id'].to_i
     peep.content = record['content']
--- spec/peep_repo_spec.rb
+++ spec/peep_repo_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PeepRepo do
     reset_test_tables
   end
 
-  it "returns a list of all peeps" do
+  it 'returns a list of all peeps' do
     repo = PeepRepo.new
 
     peeps = repo.all
@@ -18,5 +18,20 @@ RSpec.describe PeepRepo do
     expect(peeps.last.date).to eq '2023-01-03'
     expect(peeps.last.time).to eq '09:11:00'
     expect(peeps.last.author_id).to eq 1
+  end
+
+  it 'creates a new peep' do
+    repo = PeepRepo.new
+
+    peep = double(:peep, content: 'A peep', date: '2023-05-05', time: '11:21:00', author_id: 1)
+
+    repo.create(peep)
+
+    peeps = repo.all
+    expect(peeps.length).to eq 4
+    expect(peeps.first.content).to eq 'A peep'
+    expect(peeps.first.date).to eq '2023-05-05'
+    expect(peeps.first.time).to eq '11:21:00'
+    expect(peeps.first.author_id).to eq 1
   end
 end
