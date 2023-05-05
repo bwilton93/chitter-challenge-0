cc484abc2d66e0ac5b21877d71c44bcfd4f622ff
Barney <B.Wilton1993@gmail.com>
2023-05-05 10:08:53 +0100

    Wrote a failing test for all method on peeps repo

--- spec/peep_repo_spec.rb
+++ spec/peep_repo_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe PeepRepo do
     reset_test_tables
   end
 
-  it "runs a test" do
+  it "returns a list of all peeps" do
+    repo = PeepRepo.new
 
+    peeps = repo.all
+    expect(peeps.length).to eq 3
+    expect(peeps.first.content).to eq 'This is a peep'
+    expect(peeps.last.content).to eq 'This is yet another peep'
   end
 end
