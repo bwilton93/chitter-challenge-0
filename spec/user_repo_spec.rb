8ec4ebcfe5038440e5ea2ecd4d21bbdcc45e0dbe
Barney <B.Wilton1993@gmail.com>
2023-05-09 12:03:58 +0100

    Created test to fail user creation when email is not unique

--- spec/user_repo_spec.rb
+++ spec/user_repo_spec.rb
@@ -43,16 +43,17 @@ RSpec.describe UserRepo do
       expect(users.last.display_name).to eq 'User 3'
     end
     
-    # it 'fails when email is not unique' do
-    #   user = double(:user, display_name: 'User 3', username: 'user3', password: 'fake_password3', email: 'fake_email2@email.com')
-    #   repo = UserRepo.new
+    it 'fails when email is not unique' do
+      user = double(:user, display_name: 'User 3', username: 'user3', password: 'fake_password3', email: 'fake_email2@email.com')
+      repo = UserRepo.new
       
-    #   repo.create(user)
+      repo.create(user)
 
-    #   expect(users.length).to eq 2
-    #   expect(users.first.display_name).to eq 'User 1'
-    #   expect(users.last.display_name).to eq 'User 2'
-    # end
+      users = repo.all
+      expect(users.length).to eq 2
+      expect(users.first.display_name).to eq 'User 1'
+      expect(users.last.display_name).to eq 'User 2'
+    end
     
     # it 'fails when username is not unique' do
     #   user = double(:user, display_name: 'User 3', username: 'user2', password: 'fake_password3', email: 'fake_email3@email.com')
