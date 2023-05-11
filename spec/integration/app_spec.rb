c2cbd272cfd676464d4087a0a7d907463529620c
Barney <B.Wilton1993@gmail.com>
2023-05-11 11:51:03 +0100

    updated wording on login page to reflect username & email, and removed need for user to enter a name

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -31,11 +31,9 @@ RSpec.describe Application do
       )
 
       expect(response.status).to eq 200
-      expect(response.body).to include 'Please log in to post a peep!'
       expect(response.body).to include '<h1>Log in to Chitter!</h1>'
       expect(response.body).to include '<form'
-      expect(response.body).to include '<input type="text" placeholder="Name" required="required" name="name">'
-      expect(response.body).to include '<input type="text" placeholder="Email" required="required" name="email">'
+      expect(response.body).to include '<input type="text" placeholder="Enter username or email" required="required" name="login_name">'
       expect(response.body).to include '<input type="text" placeholder="Password" required="required" name="password">'
       expect(response.body).to include '<input type="submit" value="Log in!">'
     end
@@ -58,8 +56,7 @@ RSpec.describe Application do
       expect(response.status).to eq 200
       expect(response.body).to include '<h1>Log in to Chitter!</h1>'
       expect(response.body).to include '<form'
-      expect(response.body).to include '<input type="text" placeholder="Name" required="required" name="name">'
-      expect(response.body).to include '<input type="text" placeholder="Email" required="required" name="email">'
+      expect(response.body).to include '<input type="text" placeholder="Enter username or email" required="required" name="login_name">'
       expect(response.body).to include '<input type="text" placeholder="Password" required="required" name="password">'
       expect(response.body).to include '<input type="submit" value="Log in!">'
     end
